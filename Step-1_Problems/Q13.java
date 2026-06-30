8ff4b572a625253f0c2a0ce9ec57397d0f1fa9fb
Chinmay Nayak <nchinmay66@gmai.com>
2026-06-30 07:09:05 +0530

    Today's practice

---
+++ Step-1_Problems/Q13.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        int maxCount = 0 , minCount = 0;
+        
+        int currMax = scores.get(0);
+        int currMin = scores.get(0);
+        
+        for(int i=1; i<scores.size(); i++){
+            int ele = scores.get(i);
+            if(ele>currMax){
+                maxCount++;
+                currMax = ele;
+            }
+            
+            if(ele<currMin){
+                minCount++;
+                currMin = ele;
+            }
+        }
+        
+        return Arrays.asList(maxCount,minCount);
+    }
+
+}
+
+public class Q13 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
